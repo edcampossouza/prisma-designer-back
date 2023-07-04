dd5a4c07e2117f31713d13ece4719e4d57b2bfaa
Eduardo Souza <edcampossouza@gmail.com>
2023-07-04 17:31:27 -0300

    tests: schema controller

--- src/schema/schema.controller.spec.ts
+++ src/schema/schema.controller.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SchemaController } from './schema.controller';
+import { SchemaService } from './schema.service';
+import { AuthGuard } from '../auth/auth.guard';
+import { JwtService } from '@nestjs/jwt';
 
 describe('SchemaController', () => {
   let controller: SchemaController;
+  const schemaServiceMock = {};
+  const guardMock = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SchemaController],
+      providers: [
+        JwtService,
+        { provide: SchemaService, useValue: schemaServiceMock },
+      ],
     }).compile();
 
     controller = module.get<SchemaController>(SchemaController);
